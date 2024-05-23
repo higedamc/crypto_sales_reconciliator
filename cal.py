e059e9e6b64c2b555b2b4012462bb2276f04a71c
HigeMan <kohei.otani@tutanota.com>
2024-05-23 22:34:59 +0900

    Add datetime for today

--- cal.py
+++ cal.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 # 科学的表記法ではなく小数点表記を強制
 pd.set_option('display.float_format', '{:.10f}'.format)
@@ -10,30 +11,16 @@ def process_data(file_path, start_date):
     # データのフィルタリング
     filtered_df = df[(df['utcDate'] >= start_date) & (df['type'] != 'DEBIT')]
     filtered_df['formatted_date'] = pd.to_datetime(filtered_df['utcDate'])
-    filtered_df = filtered_df.sort_values(by='formatted_date', ascending=True) 
+    filtered_df = filtered_df.sort_values(by='formatted_date', ascending=True)
 
     # 合計額とfiatTotalの計算
     total_amount = filtered_df['amount'].sum()
     fiat_total = 500 * len(filtered_df[filtered_df['type'] == 'CREDIT'])
 
-    # データのフォーマット変更
-    formatted_data = []
-    for _, row in filtered_df.iterrows():
-        filtered_df = df[(df['utcDate'] >= start_date) & (df['type'] != 'DEBIT') & (df['pointOfSale'] == True)]
-        formatted_date = pd.to_datetime(row['utcDate']).strftime('%Y/%m/%d')
-        filtered_df['formatted_date'] = pd.to_datetime(filtered_df['utcDate'])
-        filtered_df = filtered_df.sort_values(by='formatted_date', ascending=True) 
-        formatted_amount = f"{row['amount']:.8f}"
-        formatted_line = f"{formatted_date}\n- {formatted_amount} / 500 JPY\ntx: https://lndecode.com/?invoice={row['address']}"
-        formatted_data.append(formatted_line)
-    # 結果の出力
-    # formatted_output = "\n\n".join(formatted_data)
-    # final_output = f"{formatted_output}\n\n計: {total_amount} BTC / {fiat_total} 円"
-    # return final_output
-
-        # マークダウンファイルの生成
+    # マークダウンファイルの生成
     with open('sales_report.md', 'w') as f:
-        f.write('# Sales Report\n\n')
+        today = datetime.now().strftime('%Y-%m-%d')  # 現在の日付を取得
+        f.write(f'# Sales Report - {today}\n\n')
         f.write('| Date       | BTC Amount | JPY Amount | Transaction Link |\n')
         f.write('|------------|------------|------------|------------------|\n')
         for _, row in filtered_df.iterrows():
@@ -47,10 +34,8 @@ def process_data(file_path, start_date):
         f.write('\n## Total Summary\n')
         f.write(f"**Total BTC:** {total_amount:.8f} BTC\n")
         f.write(f"**Total JPY:** {fiat_total} JPY\n")
-    
+
     return "Markdown report generated successfully."
-
-
 
 # ここでファイルパスと開始日を指定
 file_path = 'censored.csv'
